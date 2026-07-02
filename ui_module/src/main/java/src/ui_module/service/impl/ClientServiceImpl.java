8e4c8cd71008c4007abd6907077aecf53b765bae
Denis <Denisandreevich93@gmail.com>
2026-07-03 01:18:00 +0300

    Сервисный слой

--- ui_module/src/main/java/src/ui_module/service/impl/ClientServiceImpl.java
+++ ui_module/src/main/java/src/ui_module/service/impl/ClientServiceImpl.java
@@ -1,7 +1,10 @@
 package src.ui_module.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import src.ui_module.service.ClientService;
 
+@Service
 public class ClientServiceImpl implements ClientService {
 
 }
