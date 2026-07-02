ee44993fed1b4bb9f8ee7e3764a6a7853ff15a0d
Denis <Denisandreevich93@gmail.com>
2026-07-03 01:17:16 +0300

    Сервисный слой

---
+++ ui_module/src/main/java/src/ui_module/service/ClientService.java
@@ -0,0 +1,5 @@
+package src.ui_module.service;
+
+public interface ClientService {
+
+}
---
+++ ui_module/src/main/java/src/ui_module/service/impl/ClientServiceImpl.java
@@ -0,0 +1,7 @@
+package src.ui_module.service.impl;
+
+import src.ui_module.service.ClientService;
+
+public class ClientServiceImpl implements ClientService {
+
+}
