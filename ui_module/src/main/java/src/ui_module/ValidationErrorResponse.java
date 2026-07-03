f181a3077929c29936fc73af0bdba44c3b16930d
Denis <Denisandreevich93@gmail.com>
2026-07-04 00:56:53 +0300

    Валидация

--- ui_module/src/main/java/src/ui_module/ValidationErrorResponse.java
+++ ui_module/src/main/java/src/ui_module/ValidationErrorResponse.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ValidationErrorResponse {
--- ui_module/src/main/java/src/ui_module/ValidationException.java
+++ ui_module/src/main/java/src/ui_module/ValidationException.java
@@ -3,14 +3,13 @@ package src.ui_module;
 import java.util.List;
 
 import lombok.Getter;
-
 @Getter
 public class ValidationException extends RuntimeException {
 
-	private final List<String> errorMessages;
+    private final List<String> errorMessages;
 
-	public ValidationException(String message, List<String> errorMessages) {
-		super(message);
-		this.errorMessages = errorMessages;
-	}
+    public ValidationException(String message, List<String> errorMessages) {
+        super(message);
+        this.errorMessages = errorMessages;
+    }
 }
